3864be80422ae7332e1e325e958a5049a35386b0
Sam Skinner <sebs@Sams-MacBook-Pro-3.local>
2025-05-12 15:15:31 -0400

    added voxel world header

--- src/gfx/generators/voxel/voxel_world.hpp
+++ src/gfx/generators/voxel/voxel_world.hpp
@@ -1,7 +1,10 @@
 #pragma once
 
+#include "boost/container/small_vector.hpp"
 #include "gfx/generators/voxel/data_structures.hpp"
+#include "gfx/generators/voxel/generator.hpp"
 #include "gfx/generators/voxel/material.hpp"
+#include "gfx/generators/voxel/voxel_renderer.hpp"
 #include "util/allocators/opaque_integer_handle_allocator.hpp"
 
 namespace gfx::generators::voxel
@@ -27,11 +30,27 @@ namespace gfx::generators::voxel
         VoxelEntity       createVoxelEntity(glm::u8vec3 dimension);
         UniqueVoxelEntity createUniqueVoxelEntity(glm::u8vec3 dimension);
 
-        void setVoxelEntityContent(const VoxelEntity&, std::span<const std::pair<Voxel, ChunkLocalPosition>>);
+        void setVoxelEntityContent(const VoxelEntity&, std::vector<std::pair<Voxel, ChunkLocalPosition>>);
         void setPosition(const VoxelEntity&, WorldPosition);
 
-        void onFrameUpdate();
+        // Returns the VoxelRenderer needed for rendering
+        VoxelRenderer* onFrame();
 
     private:
+        util::OpaqueHandleAllocator<VoxelEntity> voxel_entity_allocator;
+
+        struct VoxelEntityData
+        {
+            WorldPosition                                     position;
+            glm::u8vec3                                       dimension;
+            std::vector<std::pair<Voxel, ChunkLocalPosition>> voxels;
+            boost::container::
+                flat_set<AlignedChunkCoordinate, std::less<>, boost::container::small_vector<AlignedChunkCoordinate, 8>>
+                    near_chunks;
+        };
+
+        WorldGenerator                                                        generator;
+        std::unordered_map<AlignedChunkCoordinate, VoxelRenderer::VoxelChunk> chunks;
+        VoxelRenderer                                                         voxel_renderer;
     };
 } // namespace gfx::generators::voxel
